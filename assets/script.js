2843411d9489f7fa5291a5c4cd36ac4a40497494
Alfonso James <ajaeguy@gmail.com>
2020-10-19 16:04:08 -0400

    fully functioning cpmplete draft

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,22 @@
-//set current day
+//set current day//
 n =  new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("currentDay").innerHTML = m + "/" + d + "/" + y;
 
-//color changes for time blocks
+//color changes for time blocks//
 var getTime = new Date().getHours();
 var time = parseInt(getTime);
-console.log(time);
+// console.log(time);
 
-$('.time-block').each(function(index, value) {
+// load saved events//
+$('.time-block').each(function() {
     var timeBlock = parseInt(this.id);
     if (timeBlock > 0 && timeBlock < 6 ) {
         timeBlock += 12;
        }
-    console.log(timeBlock); 
+    // console.log(timeBlock); 
     
 
     if 
@@ -31,5 +32,38 @@ $('.time-block').each(function(index, value) {
     }
   );
 
-//save button functions
+//save button functions//
+$(".saveBtn").hover(function(){
+    $(this).css('background-color', 'yellow');
+    }, function(){
+    $(this).css('background-color', '#06AED5');
+  });
 
+$('.saveBtn').each(function(index, value) {  
+
+    $(this).click (function() {
+        var textarea = $(this).siblings('textarea');
+        // var parent = $(this).parent();
+        var key = $(this).parent().attr("id");
+        var entry = $(textarea).val();
+        if (entry != "") {
+            console.log(key);
+            console.log(entry);
+            localStorage.setItem(key, entry); //store a key/value
+            var recall = localStorage.getItem(key); //retrieve the key
+            
+            return recall;
+        }
+    });
+});
+// load saved events//
+
+$( document ).ready(function() {
+    $('.saveBtn').each(function(index, value) {  
+        var key = $(this).parent().attr("id");
+        var recall = localStorage.getItem(key);
+        var textarea = $(this).siblings('textarea');
+        $(textarea).text(recall);
+        console.log(textarea);
+    })
+})
